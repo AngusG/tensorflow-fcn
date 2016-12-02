2cddffde6d3dc0c137e209856dcfb4f1a1a90adf
AngusG <premiumdriedfoods@gmail.com>
2016-12-01 23:34:36 -0500

    pull 1 ex

--- test_fcn32_vgg.py
+++ test_fcn32_vgg.py
@@ -41,71 +41,94 @@ def read_and_decode(filename_queue):
     segmentation = tf.decode_raw(features['mask_raw'], tf.uint8)
 
     image.set_shape([224*224*3])
-    segmentation.set_shape([224*224*1])
+    #segmentation.set_shape([224*224*1])
 
     image = tf.reshape(image,[224,224,3])
-    segmentation = tf.reshape(segmentation,[224,224])
+    #segmentation = tf.reshape(segmentation,[1,224,224])
     
-    return image, segmentation
+    #return image, segmentation
+    return image
 
 def input_pipeline(filenames, batch_size, num_epochs=None):
     filename_queue = tf.train.string_input_producer(
         [filenames], num_epochs=num_epochs,shuffle=False)
 
-    image, label = read_and_decode(filename_queue)
+    #image, label = read_and_decode(filename_queue)
+    image = read_and_decode(filename_queue)
 
+    '''
     images_batch, labels_batch = tf.train.batch(
         [image, label], 
-        enqueue_many=False,
+        enqueue_many=True,
         batch_size=batch_size,
         allow_smaller_final_batch=True,
         )
     return images_batch, labels_batch
+    '''
+    '''
+    images_batch = tf.train.batch(
+        [image], 
+        enqueue_many=True,
+        batch_size=batch_size,
+        allow_smaller_final_batch=True,
+        )
+    '''
+    image_batch = tf.expand_dims(image, 0)    
+    return image_batch
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--rec', help="training tfrecord file", default="input_data_ciona_crop.tfrecords")
     parser.add_argument('--npypath',help="path to weights",default="/scratch/gallowaa/")
     parser.add_argument('--ep', help="number of epochs.", type=int, default=50)
-    parser.add_argument('--bs', help="batch size", type=int, default=32)
+    parser.add_argument('--bs', help="batch size", type=int, default=1)
     parser.add_argument('--savepath',help="path to input image", \
     default="/scratch/gallowaa/224-ground-truthed/fcn32-pred/")
     #parser.add_argument('--imgpath',help="path to input image",default="/scratch/gallowaa/224-ground-truthed/images/")
     args = parser.parse_args()
 
     print args.npypath+args.rec
-    batch_images, batch_segmentations = input_pipeline(args.npypath+args.rec, args.bs)
 
-    init = tf.initialize_all_variables()
-    init_locals = tf.initialize_local_variables()
+
+    with tf.Graph().as_default():
+
+        #batch_images, batch_segmentations = input_pipeline(args.npypath+args.rec, args.bs)
+        single_image = input_pipeline(args.npypath+args.rec, args.bs)
+
+        init = tf.initialize_all_variables()
+        init_locals = tf.initialize_local_variables()
     
-    with tf.Session() as sess:
+        with tf.Session() as sess:
 
-        #images = tf.placeholder("float")
-        #feed_dict = {images: img1}
-        #batch_images = tf.expand_dims(images, 0)
+            sess.run([init, init_locals])
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(coord=coord)
 
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(coord=coord)
-        sess.run([init, init_locals])
+            vgg_fcn = fcn32_vgg.FCN32VGG(args.npypath+"vgg16.npy")
 
-        vgg_fcn = fcn32_vgg.FCN32VGG(args.npypath+"vgg16.npy")
-        
-        #with tf.name_scope("content_vgg"):
-        vgg_fcn.build(batch_images, debug=True)
+            #with tf.name_scope("content_vgg"):
+            vgg_fcn.build(batch_images, debug=True)
 
-        print('Finished building Network.')        
+            print('Finished building Network.')     
+            print('Running the Network')
 
-        print('Running the Network')
-        tensors = [vgg_fcn.pred, vgg_fcn.pred_up]
-        #down, up = sess.run(tensors, feed_dict=feed_dict)
-        down, up = sess.run(tensors)
+            for i in range(50):
+                
+                try:
+                    img = sess.run([single_image])   
 
-        #down_color = utils.color_image(down[0])
-        #up_color = utils.color_image(up[0])
+                    tensors = [vgg_fcn.pred, vgg_fcn.pred_up]
+                    down, up = sess.run(tensors, feed_dict=feed_dict)                   
 
-        #scp.misc.imsave('fcn32_downsampled.png', down_color)
-        #scp.misc.imsave('fcn32_upsampled.png', up_color)
-        scp.misc.imsave(args.savepath+'fcn32_upsampled_'+str(i)+'.png', up_color)
-        coord.request_stop()
-        coord.join(threads)
+                    #down_color = utils.color_image(down[0])
+                    #up_color = utils.color_image(up[0])
+
+                    #scp.misc.imsave('fcn32_downsampled.png', down_color)
+                    #scp.misc.imsave('fcn32_upsampled.png', up_color)
+                    scp.misc.imsave(args.savepath+'fcn32_upsampled_'+str(i)+'.png', up_color)
+
+                except tf.errors.OutOfRangeError:
+                    print('Done training -- epoch limit reached')
+                finally:
+                    coord.request_stop()
+                    coord.join(threads)
